ed11a978447818b9476cf804e4f890d87d111ca3
WCeasar <285665110@qq.com>
2023-07-01 02:39:12 +0800

    feat: graphql 初步集成前端

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,64 @@
+import { ChangeEvent, useState } from 'react';
+import { useMutation, useQuery } from '@apollo/client';
 import './App.css';
+import { FIND, UPDATE } from './graphql/demo';
 
-function App() {
-  return <>react</>;
-}
+const App = () => {
+  const [name, setName] = useState('');
+  const [desc, setDesc] = useState('');
+
+  const { data, loading } = useQuery(FIND, {
+    variables: {
+      id: '21dac973-9aee-4662-aea2-8278be59ba94',
+    },
+  });
+
+  const onNameInputChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
+    setName(e.target.value);
+  };
+
+  const onDescInputChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
+    setDesc(e.target.value);
+  };
+
+  const [update] = useMutation(UPDATE);
+
+  const handleSubmit = () => {
+    update({
+      variables: {
+        id: '21dac973-9aee-4662-aea2-8278be59ba94',
+        params: {
+          name,
+          desc,
+        },
+      },
+    });
+  };
+  return (
+    <>
+      <p>{JSON.stringify(data)}</p>
+      <p>{`${loading}`}</p>
+
+      <p>
+        <input type="text" onChange={onNameInputChangeHandler} />
+      </p>
+
+      <p>
+        <input type="text" onChange={onDescInputChangeHandler} />
+      </p>
+
+      <p>
+        <button
+          type="button"
+          onClick={() => {
+            handleSubmit();
+          }}
+        >
+          提交
+        </button>
+      </p>
+    </>
+  );
+};
 
 export default App;
